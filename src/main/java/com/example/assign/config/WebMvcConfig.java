30941c6c8a7ef120e4a5467c608343f79e83e603
Duy Nam <hoangduynam20996@gmail.com>
2023-08-25 16:43:44 +0700

    Edit class config

--- src/main/java/com/example/assign/config/WebMvcConfig.java
+++ src/main/java/com/example/assign/config/WebMvcConfig.java
@@ -10,7 +10,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:8080", "https://example-service-three.onrender.com")
+                .allowedOrigins("http://127.0.0.1:5500",
+                        "http://localhost:8080",
+                        "https://example-service-three.onrender.com",
+                        "https://web-tiki-fake.000webhostapp.com"
+                )
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(false)
